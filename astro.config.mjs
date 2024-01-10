9eff583d04b8068cf4e8f3e69c58b4343c326120
Aleksandr Medvedev <a.medvedev@liga-team.ru>
2024-01-10 08:16:59 +0300

    site

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ import icon from 'astro-icon'
 // https://astro.build/config
 export default defineConfig({
   devToolbar: { enabled: false },
-  site: 'https://exteep.github.io/astro-test',
+  site: 'https://lorganium.ru',
   compressHTML: false,
   output: 'static',
   publicDir: './public',
